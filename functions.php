a2040cfc5540e3396d056c157ffc3e898e69637c
dikiyforester <frolov.en@gmail.com>
2020-04-26 12:22:30 +0500

    Initial commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Theme functions file
+ *
+ * DO NOT MODIFY THIS FILE. Make a child theme instead: http://codex.wordpress.org/Child_Themes
+ *
+ * @package RefPress
+ * @author  ArThemes
+ * @since   1.0.0
+ */
+
+define( 'RP_WPORG_DIR', 'wporg-developer' );
+
+/**
+ * Load wporg-developer theme as dependency.
+ */
+require __DIR__ . '/' . RP_WPORG_DIR . '/functions.php';
